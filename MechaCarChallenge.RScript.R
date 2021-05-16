03e774569d2253dcafb4f72716448b73fd896732
daniethecreator <daniedrogos@gmail.com>
2021-05-16 11:25:05 -0700

    Challenge 15

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -6,7 +6,9 @@ cor(mechaCars_matrix)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCars) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,MechaCars)) 
 Suspension <- read.csv('Suspension_Coil.csv',stringsAsFactors = F) #read in dataset
-#dplyr::group_by(Manufacturing_Lot) %>%
-#total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
-#Suspension %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+t.test(Suspension$PSI,mu = 1500)
+t.test(subset(Suspension,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+t.test(subset(Suspension,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+t.test(subset(Suspension,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
